191a6f90352098ab74a2cd77d8fb51d89c58938c
madvirus <madvirus@madvirus.net>
2021-03-31 11:04:20 +0900

    feat: support type added - LocalDateTime, LocalTime

--- src/main/java/cucumbertables/MapRowWrap.java
+++ src/main/java/cucumbertables/MapRowWrap.java
@@ -2,6 +2,8 @@ package cucumbertables;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
@@ -22,33 +24,58 @@ public class MapRowWrap {
         return nullString.equals(value) ? null : value;
     }
 
+    private boolean isNullOrEmpty(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+
     public Integer getInteger(String colName) {
         String value = getValue(colName);
-        return value == null || value.trim().isEmpty() ? null : Integer.valueOf(value);
+        return isNullOrEmpty(value) ? null : Integer.valueOf(value.replace(",", ""));
     }
 
     public Double getDouble(String colName) {
         String value = getValue(colName);
-        return value == null || value.trim().isEmpty() ? null : Double.valueOf(value);
+        return isNullOrEmpty(value) ? null : Double.valueOf(value);
     }
 
     public Long getLong(String colName) {
         String value = getValue(colName);
-        return value == null || value.trim().isEmpty() ? null : Long.valueOf(value);
+        return isNullOrEmpty(value) ? null : Long.valueOf(value);
     }
 
     public BigDecimal getBigDecimal(String colName) {
         String value = getValue(colName);
-        return value == null || value.trim().isEmpty() ? null : BigDecimal.valueOf(Double.valueOf(value));
+        return isNullOrEmpty(value) ? null : BigDecimal.valueOf(Double.valueOf(value));
     }
 
     public String getString(String colName) {
-        String value = getValue(colName);
-        return value;
+        return getValue(colName);
     }
 
     public LocalDate getLocalDate(String colName, String pattern) {
         String value = getValue(colName);
-        return value == null || value.trim().isEmpty() ? null : LocalDate.parse(value.trim(), DateTimeFormatter.ofPattern(pattern));
+        return isNullOrEmpty(value) ? null : LocalDate.parse(value.trim(), DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public LocalDate getLocalDate(String colName) {
+        return getLocalDate(colName, "yyyy-MM-dd");
+    }
+
+    public LocalDateTime getLocalDateTime(String colName, String pattern) {
+        String value = getValue(colName);
+        return isNullOrEmpty(value) ? null : LocalDateTime.parse(value.trim(), DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public LocalDateTime getLocalDateTime(String colName) {
+        return getLocalDateTime(colName, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    public LocalTime getLocalTime(String colName, String pattern) {
+        String value = getValue(colName);
+        return isNullOrEmpty(value) ? null : LocalTime.parse(value.trim(), DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public LocalTime getLocalTime(String colName) {
+        return getLocalTime(colName, "HH:mm:ss");
     }
 }
--- src/test/java/cucumbertables/DataTableWrapTest.java
+++ src/test/java/cucumbertables/DataTableWrapTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -42,8 +44,8 @@ public class DataTableWrapTest {
     @Test
     void tableWrapNumber() {
         DataTable dataTable = DataTable.create(Arrays.asList(
-                Arrays.asList("integer", "double", "long"),
-                Arrays.asList("1", "0.123", "12345")
+                Arrays.asList("integer", "double", "long", "comma"),
+                Arrays.asList("1", "0.123", "12345", "12,345")
         ));
         DataTableWrap table = DataTableWrap.create(dataTable);
         MapRowWrap row = table.getMapRows().get(0);
@@ -51,6 +53,46 @@ public class DataTableWrapTest {
         assertThat(row.getDouble("double")).isEqualTo(Double.valueOf(0.123));
         assertThat(row.getLong("long")).isEqualTo(Long.valueOf(12345));
         assertThat(row.getBigDecimal("double")).isEqualTo(BigDecimal.valueOf(0.123));
+        assertThat(row.getInteger("comma")).isEqualTo(Integer.valueOf(12345));
+    }
+
+    @Test
+    void tableWrapLocalDate() {
+        DataTable dataTable = DataTable.create(Arrays.asList(
+                Arrays.asList("date1", "date2"),
+                Arrays.asList("2021-03-31", "20210331")
+        ));
+        DataTableWrap table = DataTableWrap.create(dataTable);
+        MapRowWrap row = table.getMapRows().get(0);
+        assertThat(row.getLocalDate("date1")).isEqualTo(LocalDate.of(2021, 3, 31));
+        assertThat(row.getLocalDate("date1", "yyyy-MM-dd")).isEqualTo(LocalDate.of(2021, 3, 31));
+        assertThat(row.getLocalDate("date2", "yyyyMMdd")).isEqualTo(LocalDate.of(2021, 3, 31));
+    }
+
+    @Test
+    void tableWrapLocalDateTime() {
+        DataTable dataTable = DataTable.create(Arrays.asList(
+                Arrays.asList("datetime1", "datetime2"),
+                Arrays.asList("2021-03-31 14:50:15", "20210331145015")
+        ));
+        DataTableWrap table = DataTableWrap.create(dataTable);
+        MapRowWrap row = table.getMapRows().get(0);
+        assertThat(row.getLocalDateTime("datetime1")).isEqualTo(LocalDateTime.of(2021, 3, 31, 14, 50, 15));
+        assertThat(row.getLocalDateTime("datetime1", "yyyy-MM-dd HH:mm:ss")).isEqualTo(LocalDateTime.of(2021, 3, 31, 14, 50, 15));
+        assertThat(row.getLocalDateTime("datetime2", "yyyyMMddHHmmss")).isEqualTo(LocalDateTime.of(2021, 3, 31, 14, 50, 15));
+    }
+
+    @Test
+    void tableWrapLocalTime() {
+        DataTable dataTable = DataTable.create(Arrays.asList(
+                Arrays.asList("time1", "time2"),
+                Arrays.asList("14:50:15", "145015")
+        ));
+        DataTableWrap table = DataTableWrap.create(dataTable);
+        MapRowWrap row = table.getMapRows().get(0);
+        assertThat(row.getLocalTime("time1")).isEqualTo(LocalTime.of(14, 50, 15));
+        assertThat(row.getLocalTime("time1", "HH:mm:ss")).isEqualTo(LocalTime.of(14, 50, 15));
+        assertThat(row.getLocalTime("time2", "HHmmss")).isEqualTo(LocalTime.of(14, 50, 15));
     }
 
     @Test
